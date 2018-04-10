50404e955216e323120568f2cf7789960b6f7714
wutian <wutian@zafh.com.cn>
2018-04-10 19:17:47 +0800

    练习3

---
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise3.java
@@ -0,0 +1,22 @@
+package com.wt.algorithm4.chapter1_1;
+
+import java.util.Scanner;
+
+/**
+ * @Author WuTian
+ * @Date 2018-04-10 16:52
+ */
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(true){
+            System.out.println("Please input a integer:");
+            int a = in.nextInt();
+            System.out.println("Please input a integer:");
+            int b = in.nextInt();
+            System.out.println("Please input a integer:");
+            int c = in.nextInt();
+            System.out.println(a==b&&a==c);
+        }
+    }
+}
