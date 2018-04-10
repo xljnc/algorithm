d1e3dbab5f827e89db3eeb0d9d82d20c3fc027fd
wutian <wutian@zafh.com.cn>
2018-04-10 12:59:10 +0800

    initial

---
+++ src/test/java/com/wt/algorithm4/Algorithm4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wt.algorithm4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Algorithm4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
