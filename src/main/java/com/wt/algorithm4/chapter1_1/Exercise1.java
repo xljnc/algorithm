c395e301a0891b2b381518530a039eb5795fa778
wutian <wutian@zafh.com.cn>
2018-04-10 19:14:09 +0800

    练习1

---
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise1.java
@@ -0,0 +1,13 @@
+package com.wt.algorithm4.chapter1_1;
+
+/**
+ * @Author WuTian
+ * @Date 2018-04-10 16:20
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println((0+15)/2);
+        System.out.println(2.0e-6*100000000.1);
+        System.out.println(true && false || true && true);
+    }
+}
