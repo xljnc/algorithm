b0e48d38d1670d038c2c7b12e859bbe3f89bae15
wutian <wutian@zafh.com.cn>
2018-04-10 19:16:02 +0800

    练习2

---
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise2.java
@@ -0,0 +1,14 @@
+package com.wt.algorithm4.chapter1_1;
+
+/**
+ * @Author WuTian
+ * @Date 2018-04-10 16:51
+ */
+public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println((1 + 2.236) / 2);
+        System.out.println(1 + 2 + 3 + 4.0);
+        System.out.println(4.1 >= 4);
+        System.out.println(1 + 2 + "3");
+    }
+}
