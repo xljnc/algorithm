597c21c93055e348f424fec685c2789479291ef6
wutian <wutian@zafh.com.cn>
2018-04-11 09:31:28 +0800

    练习1.1.20

---
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise20.java
@@ -0,0 +1,33 @@
+package com.wt.algorithm4.chapter1_1;
+
+import com.sun.media.sound.SoftTuning;
+
+import java.util.Scanner;
+
+/**
+ * @Author WuTian
+ * @Date 2018-04-11 9:14
+ */
+public class Exercise20 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = 0;
+        while (true) {
+            System.out.println("Please input a integer which is bigger than 0.");
+            n = in.nextInt();
+            if(n<=0){
+                System.out.println("Invalid input.");
+                continue;
+            }
+            System.out.println("result of ln(N!) is:"+calcLn(n));
+        }
+    }
+
+    public static double calcLn(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        return Math.log(n) + calcLn(n - 1);
+    }
+
+}
