6139a204b31f1af98be7bd2ba433ef3c83b596ed
wutian <wutian@zafh.com.cn>
2018-04-18 19:42:44 +0800

    练习1.1.13

--- src/main/java/com/wt/algorithm4/chapter1_1/Exercise13.java
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise13.java
@@ -1,20 +1,25 @@
 package com.wt.algorithm4.chapter1_1;
 
+/**
+ * @Author WuTian
+ * @Date 2018-04-10 19:09
+ */
 public class Exercise13 {
-    public static void printMatrix(int[][] matrix){
-        for(int i=0;i<matrix[0].length;i++){
-            for(int j=0;j<matrix.length;j++){
-                if(j!=0){
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (j != 0) {
                     System.out.print(" ");
                 }
-               System.out.print(matrix[j][i]);
+                System.out.print(matrix[j][i]);
             }
             System.out.println();
         }
 
     }
+
     public static void main(String[] args) {
-        int[][] a = { { 100, 200, 300 }, { 400, 500, 600 } };
+        int[][] a = {{100, 200, 300}, {400, 500, 600}};
         printMatrix(a);
     }
 }
