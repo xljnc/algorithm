5c2c02046b211f40ca91cfcf3ff7a571c542d566
wutian <wutian@zafh.com.cn>
2018-04-18 19:42:23 +0800

    练习1.1.12

--- src/main/java/com/wt/algorithm4/chapter1_1/Exercise12.java
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise12.java
@@ -1,5 +1,9 @@
 package com.wt.algorithm4.chapter1_1;
 
+/**
+ * @Author WuTian
+ * @Date 2018-04-10 19:09
+ */
 public class Exercise12 {
     public static void main(String[] args) {
         int[] a = new int[10];
@@ -9,11 +13,10 @@ public class Exercise12 {
         for (int i = 0; i < 10; i++) {
             a[i] = a[a[i]];
         }
-        // I don't understand why the author asks this
+        //应该是印刷错误
 //        for (int i = 0; i < 10; i++) {
 //            System.out.println(i);
 //        }
-        // I think the author should ask this
         for (int i = 0; i < 10; i++) {
             System.out.println(a[i]);
         }
