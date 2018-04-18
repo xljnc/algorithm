d43d19d367004128e1649fd306a9d3127c7a0bd3
wutian <wutian@zafh.com.cn>
2018-04-18 19:02:14 +0800

    练习1.1.19

--- src/main/java/com/wt/algorithm4/chapter1_1/Exercise19.java
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise19.java
@@ -7,10 +7,33 @@ import java.math.BigDecimal;
  * @Date 2018-04-13 17:59
  */
 public class Exercise19 {
-    public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            System.out.println(i + " " + fibonacci(i));
+    private static BigDecimal[] result = new BigDecimal[10000];
+    private static final BigDecimal initObject = new BigDecimal(-1);
+
+    static {
+        result[0] = new BigDecimal(0);
+        result[1] = new BigDecimal(1);
+        for (int i = 2; i < result.length; i++) {
+            result[i] = initObject;
         }
+    }
+
+    public static void main(String[] args) {
+        /*
+         *不使用缓存，计算速度非常慢
+         */
+//        long start = System.currentTimeMillis();
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(i + " " + fibonacci(i));
+//        }
+//        long end = System.currentTimeMillis();
+//        System.out.println((end - start) / 1000);
+        long cStart = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(i + " " + fibonacciUsingCache(i));
+        }
+        long cEnd = System.currentTimeMillis();
+        System.out.println((cEnd - cStart) / 1000);
     }
 
     public static BigDecimal fibonacci(int n) {
@@ -21,6 +44,22 @@ public class Exercise19 {
             return new BigDecimal(1);
         }
         return fibonacci(n - 1).add(fibonacci(n - 2));
+    }
 
+    public static BigDecimal fibonacciUsingCache(int n) {
+        if (n == 0) {
+            return result[0];
+        }
+        if (n == 1) {
+            return result[1];
+        }
+        if (n >= result.length) {
+            return fibonacciUsingCache(n - 1).add(fibonacciUsingCache(n - 2));
+        } else if (result[n].equals(initObject)) {
+            result[n] = fibonacciUsingCache(n - 1).add(fibonacciUsingCache(n - 2));
+            return result[n];
+        } else {
+            return result[n];
+        }
     }
 }
