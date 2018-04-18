3667bc2d031170bf18eae39b63402af0943af517
wutian <wutian@zafh.com.cn>
2018-04-18 19:36:57 +0800

    练习1.1.7C

--- src/main/java/com/wt/algorithm4/chapter1_1/Exercise07C.java
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise07C.java
@@ -1,5 +1,9 @@
 package com.wt.algorithm4.chapter1_1;
 
+/**
+ * @Author WuTian
+ * @Date 2018-04-10 19:09
+ */
 public class Exercise07C {
     public static void main(String[] args) {
         int sum = 0;
