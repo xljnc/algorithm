9bc053076ecba11b74b590d5f003ccea2179c873
wutian <wutian@zafh.com.cn>
2018-05-31 19:02:26 +0800

    Data Structure & Algorithms in Java 二分查找，使用递归 by Xljnc 2018-May-31

---
+++ src/main/java/com/wt/dsaainjava/chapter2/BinarySearchUsingRecursion.java
@@ -0,0 +1,34 @@
+package com.wt.dsaainjava.chapter2;
+
+import java.util.Arrays;
+
+/**
+ * @author WuTian
+ * @date 2018-05-31 18:53
+ * @description
+ */
+public class BinarySearchUsingRecursion {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 13, 16, 25, 26, 35, 36, 42, 49, 50, 60, 63, 68, 77, 83, 93, 96, 98};
+        System.out.println(Arrays.toString(a));
+        int index = binarySearch(a, 36, 0, a.length);
+        System.out.println((index == -1) ? "Key not found!" : ("Key at index " + index));
+    }
+
+    public static int binarySearch(int[] src, int searchKey, int start, int end) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            if (searchKey == src[mid]) {
+                return mid;
+            } else if (searchKey < src[mid]) {
+                return (binarySearch(src, searchKey, start, mid - 1));
+            } else {
+                return (binarySearch(src, searchKey, mid + 1, end));
+            }
+        } else {
+            return -1;
+        }
+    }
+
+}
