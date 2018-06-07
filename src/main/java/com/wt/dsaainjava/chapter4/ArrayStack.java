002745ab49736985aee72d1cf5bcf02cc3561044
wutian <wutian@zafh.com.cn>
2018-06-07 10:58:38 +0800

    Data Structure & Algorithms in Java 章节4 栈实现 by Xljnc 2018-Jun-07

--- src/main/java/com/wt/dsaainjava/chapter4/ArrayStack.java
+++ src/main/java/com/wt/dsaainjava/chapter4/ArrayStack.java
@@ -1,65 +1,137 @@
 package com.wt.dsaainjava.chapter4;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author WuTian
  * @date 2018-06-06 15:55
- * @description 栈数据结构，使用数组实现
+ * @description Stack implements by array , do not support slot extend
  */
 public class ArrayStack<E> implements Stack<E> {
+    private static final Logger log = LoggerFactory.getLogger(ArrayStack.class);
 
-    //数据数组
-    private E[] data;
-    //当前指针位置
-    private int position;
-    //数据数组大小
-    private int size;
+    //data array to store the elements
+    private Object[] data;
+    //a pointer indicates the current position of element
+    private int position = -1;
+    //default capacity of data array
+    private final int defaultCapacity = 10;
+    //capacity of data array
+    private int capacity;
 
-    /*
-     * @author  WuTian
-     * @date 2018/6/6 17:36
-     * @param e
-     * @return void
+    /**
+     * @param
+     * @return
      * @throws
-     * @description 将元素入栈
+     * @author WuTian
+     * @date 2018/6/7 10:56
+     * @description initialize the data array using the default capacity
+     */
+    public ArrayStack() {
+        capacity = defaultCapacity;
+        data = new Object[capacity];
+    }
+
+    /**
+     * @param capacity
+     * @return
+     * @throws
+     * @author WuTian
+     * @date 2018/6/7 10:57
+     * @description initialize the data array using the capacity given
+     */
+    public ArrayStack(int capacity) {
+        this.capacity = capacity;
+        data = new Object[this.capacity];
+    }
+
+    /**
+     * @param e
+     * @return true if element is successfully pushed to stack
+     * @throws
+     * @author WuTian
+     * @date 2018/6/7 9:36
+     * @description push the element to stack
      */
     @Override
-    public void push(E e) {
-
+    public boolean push(E e) {
+        if (isFull()) {
+            log.error("Stack is full.Can not push.");
+            return false;
+        }
+        data[++position] = e;
+        return true;
     }
 
-    private boolean isEmpty() {
-        return false;
-    }
-
-    private boolean isFull() {
-        return false;
-    }
-
-
-    /*
-     * @author  WuTian
-     * @date 2018/6/6 17:37
+    /**
      * @param
-     * @return E
+     * @return true if no element in current array
      * @throws
-     * @description 将栈顶元素出栈
+     * @author WuTian
+     * @date 2018/6/7 10:07
+     * @description return true if no element in current array
+     */
+    @Override
+    public boolean isEmpty() {
+        return position == -1;
+    }
+
+    /**
+     * @param
+     * @return true if no more slot in current array
+     * @throws
+     * @author WuTian
+     * @date 2018/6/7 10:08
+     * @description return true if no more slot in current array
+     */
+    @Override
+    public boolean isFull() {
+        return position == (capacity - 1);
+    }
+
+
+    /**
+     * @param
+     * @return E the element from top of the stack
+     * @throws
+     * @author WuTian
+     * @date 2018/6/6 17:37
+     * @description pop the element from top of the stack and remove it
      */
     @Override
     public E pop() {
-        return null;
+        if (isEmpty()) {
+            log.error("Stack is empty.Can not pop.");
+            return null;
+        }
+        return (E) data[position--];
     }
 
-    /*
-     * @author  WuTian
-     * @date 2018/6/6 17:39
+    /**
      * @param
-     * @return E
+     * @return the element from top of the stack
      * @throws
-     * @description 返回栈顶元素
+     * @author WuTian
+     * @date 2018/6/6 17:39
+     * @description return the element from top of the stack , will not remove it
      */
     @Override
     public E peek() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        return (E) data[position];
     }
 
+    public static void main(String[] args) {
+        Stack<Integer> stack = new ArrayStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
 }
--- src/main/java/com/wt/dsaainjava/chapter4/Stack.java
+++ src/main/java/com/wt/dsaainjava/chapter4/Stack.java
@@ -1,39 +1,59 @@
 package com.wt.dsaainjava.chapter4;
 
-/*
- * @author  WuTian
+/**
+ * @author WuTian
  * @date 2018/6/6 17:37
- * @description Stack数据结构顶级接口
+ * @description Top level interface of Stack
  */
 public interface Stack<E> {
 
-    /*
-     * @author  WuTian
-     * @date 2018/6/6 17:36
+    /**
      * @param e
-     * @return void
+     * @return true if element is successfully pushed to stack
      * @throws
-     * @description 将元素入栈
+     * @author WuTian
+     * @date 2018/6/7 9:36
+     * @description push the element to stack
      */
-    void push(E e);
+    boolean push(E e);
 
-    /*
-     * @author  WuTian
-     * @date 2018/6/6 17:37
+    /**
      * @param
-     * @return E
+     * @return true if no element in current array
      * @throws
-     * @description 将栈顶元素出栈
+     * @author WuTian
+     * @date 2018/6/7 10:07
+     * @description return true if no element in current array
+     */
+    boolean isEmpty();
+
+    /**
+     * @param
+     * @return true if no more slot in current array
+     * @throws
+     * @author WuTian
+     * @date 2018/6/7 10:08
+     * @description return true if no more slot in current array
+     */
+    boolean isFull();
+
+    /**
+     * @param
+     * @return E the element from top of the stack
+     * @throws
+     * @author WuTian
+     * @date 2018/6/6 17:37
+     * @description pop the element from top of the stack and remove it
      */
     E pop();
 
-    /*
-     * @author  WuTian
-     * @date 2018/6/6 17:39
+    /**
      * @param
-     * @return E
+     * @return the element from top of the stack
      * @throws
-     * @description 返回栈顶元素
+     * @author WuTian
+     * @date 2018/6/6 17:39
+     * @description return the element from top of the stack , will not remove it
      */
     E peek();
 
