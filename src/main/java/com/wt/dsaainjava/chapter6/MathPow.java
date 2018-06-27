73dc82bad8c2683b37923a58e1e4c258c5999ac3
wutian <wutian@zafh.com.cn>
2018-06-27 16:35:03 +0800

    Data Structure & Algorithms in Java 章节6 数乘方实现 by Xljnc 2018-Jun-27

---
+++ src/main/java/com/wt/dsaainjava/chapter6/MathPow.java
@@ -0,0 +1,21 @@
+package com.wt.dsaainjava.chapter6;
+
+/**
+ * @author WuTian
+ * @date 2018-06-27 16:28
+ * @description
+ */
+public class MathPow {
+    public static void main(String[] args) {
+        System.out.println(pow(4,8)==Math.pow(4,8));
+    }
+
+    private static long pow(int x, int y) {
+        if (y == 1)
+            return x;
+        if (y % 2 == 0)
+            return pow(x * x, y / 2);
+        else
+            return x * pow(x * x, (y - 1) / 2);
+    }
+}
