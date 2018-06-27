a133bb359d204b0a30bc2c5c8f52d7d889a7d37d
wutian <wutian@zafh.com.cn>
2018-06-27 14:36:26 +0800

    算法4 章节1-1 练习07 by Xljnc 2018-Jun-27

--- src/main/java/com/wt/algorithm4/chapter1_1/Exercise07A.java
+++ src/main/java/com/wt/algorithm4/chapter1_1/Exercise07A.java
@@ -10,6 +10,6 @@ public class Exercise07A {
         while (Math.abs(t - 9.0 / t) > .001) {
             t = (9.0 / t + t) / 2.0;
         }
-        System.out.printf("%.5f\n", t);
+        System.out.printf("%.5f%n", t);
     }
 }
