dcd623140e884fcf9090c64e934a15d74e9da1f7
wutian <wutian@zafh.com.cn>
2018-06-28 17:12:50 +0800

    Data Structure & Algorithms in Java 章节7 划分实现 by Xljnc 2018-Jun-28

--- src/main/java/com/wt/dsaainjava/chapter7/Partition.java
+++ src/main/java/com/wt/dsaainjava/chapter7/Partition.java
@@ -11,10 +11,40 @@ public class Partition {
     public static void main(String[] args) {
         src = new int[]{3, 5, 70, 71, 74, 43, 93, 12, 46, 15, 51, 20, 95, 56, 57, 25, 60, 30, 94};
         int pivot = 50;
-        partition(pivot);
+        System.out.println(partition(pivot));
+        display();
     }
 
-    private static void partition(int pivot) {
+    private static int partition(int pivot) {
+        int left = 0;
+        int right = src.length - 1;
+        while (true) {
+            while (left < right && src[left] <= pivot) {
+                left++;
+            }
+            while (right > left && src[right] >= pivot) {
+                right--;
+            }
+            if (left >= right)
+                break;
+            swap(left, right);
+        }
+        return left;
+    }
 
+    private static void swap(int i, int j) {
+        int temp = src[i];
+        src[i] = src[j];
+        src[j] = temp;
+    }
+
+    private static void display() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < src.length; i++) {
+            if (i != 0)
+                sb.append(" ");
+            sb.append(src[i]);
+        }
+        System.out.println(sb.toString());
     }
 }
